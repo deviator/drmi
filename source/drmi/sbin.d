30fc5eba9b9a77eeaee8184fc2c59fcef283b470
deviator <code.viator@gmail.com>
2017-08-16 19:27:29 +0300

    add readable deserialize errors

--- source/drmi/sbin.d
+++ source/drmi/sbin.d
@@ -3,11 +3,40 @@ module drmi.sbin;
 import std.algorithm;
 import std.array;
 import std.bitmanip;
+import std.exception : enforce, assertThrown;
+import std.string : format;
 import std.traits;
 import std.range;
 
+alias length_t = ulong;
 alias pack = nativeToLittleEndian;
 alias unpack = littleEndianToNative;
+
+class SBinException : Exception
+{
+    this(string msg, string file=__FILE__, size_t line=__LINE__)
+    { super(msg, file, line); }
+}
+
+class SBinDeserializeException : SBinException
+{
+    string mainType, fieldName, fieldType;
+    size_t readed, expected, fullReaded;
+    this(string mainType, string fieldName, string fieldType,
+         size_t readed, size_t expected, size_t fullReaded)
+    {
+        this.mainType = mainType;
+        this.fieldName = fieldName;
+        this.fieldType = fieldType;
+        this.readed = readed;
+        this.expected = expected;
+        this.fullReaded = fullReaded;
+        super(format("empty input range while "~
+                "deserialize '%s' element %s:%s %d/%d (readed/expected), "~
+                "readed message %d bytes", mainType, fieldName,
+                fieldType, readed, expected, fullReaded));
+    }
+}
 
 void sbinSerialize(T, R)(auto ref const T val, ref R r)
     if (isOutputRange!(R, ubyte))
@@ -18,12 +47,12 @@ void sbinSerialize(T, R)(auto ref const T val, ref R r)
         foreach (v; val) sbinSerialize(v, r);
     else static if (isSomeString!T)
     {
-        r.put((cast(ulong)val.length).pack[]);
+        r.put((cast(length_t)val.length).pack[]);
         r.put(cast(ubyte[])val);
     }
     else static if (isDynamicArray!T)
     {
-        r.put((cast(ulong)val.length).pack[]);
+        r.put((cast(length_t)val.length).pack[]);
         foreach (v; val) sbinSerialize(v, r);
     }
     else static if (is(T == struct) || isTypeTuple!T)
@@ -39,55 +68,62 @@ ubyte[] sbinSerialize(T)(auto ref const T val)
     return buf.data.dup;
 }
 
-Tb sbinDeserialize(Tb, Rb)(Rb r)
+Tb sbinDeserialize(Tb, R)(R range)
 {
-    auto impl(T,R)(ref R r)
+    size_t cnt;
+
+    ubyte pop(ref R rng, lazy string field, lazy string type,
+                lazy size_t vcnt, lazy size_t vexp)
     {
+        enforce (!rng.empty, new SBinDeserializeException(Tb.stringof,
+                                        field, type, vcnt, vexp, cnt));
+        auto ret = rng.front;
+        rng.popFront();
+        cnt++;
+        return ret;
+    }
+
+    auto impl(T)(ref R r, lazy string field)
+    {
+        string ff(lazy string n) { return field ~ "." ~ n; }
+        string fi(size_t i) { return field ~ format("[%d]", i); }
+
         static if (is(T : double) || is(T : long))
         {
             ubyte[T.sizeof] tmp;
-            foreach (ref v; tmp)
-            {
-                v = r.front;
-                r.popFront();
-            }
+            foreach (i, ref v; tmp) v = pop(r, field, T.stringof, i, T.sizeof);
             return tmp.unpack!T;
         }
         else static if (isSomeString!T)
         {
-            auto length = cast(size_t)impl!ulong(r);
+            auto length = cast(size_t)impl!length_t(r, ff("length"));
             auto tmp = new ubyte[](length);
-            foreach (ref v; tmp)
-            {
-                v = r.front;
-                r.popFront();
-            }
+            foreach (i, ref v; tmp) v = pop(r, fi(i), T.stringof, i, length);
             return cast(T)tmp;
         }
         else static if (isStaticArray!T)
         {
             T ret;
-            foreach (ref v; ret) v = impl!(typeof(ret[0]))(r);
+            foreach (i, ref v; ret) v = impl!(typeof(ret[0]))(r, fi(i));
             return ret;
         }
         else static if (isDynamicArray!T)
         {
             T ret;
-            ret.length = cast(size_t)impl!ulong(r);
-            foreach (ref v; ret) v = impl!(typeof(ret[0]))(r);
+            ret.length = cast(size_t)impl!length_t(r, ff("length"));
+            foreach (i, ref v; ret) v = impl!(typeof(ret[0]))(r, fi(i));
             return ret;
         }
         else static if (is(T == struct) || isTypeTuple!T)
         {
             T ret;
             foreach (i, ref v; ret.tupleof)
-                v = impl!(Unqual!(typeof(v)))(r);
+                v = impl!(Unqual!(typeof(v)))(r, ff(__traits(identifier, ret.tupleof[i])));
             return ret;
         }
-        else
-            static assert(0, "unsupported type: " ~ T.stringof);
+        else static assert(0, "unsupported type: " ~ T.stringof);
     }
-    return impl!Tb(r);
+    return impl!Tb(range, "");
 }
 
 unittest
@@ -168,4 +204,12 @@ unittest
     Parameters!foo b;
     b = sa.sbinDeserialize!(typeof(tuple(b)));
     assert(a == b);
+}
+
+unittest
+{
+    auto a = [1,2,3,4];
+    auto as = a.sbinSerialize;
+    auto as_tr = as[0..17];
+    assertThrown!SBinDeserializeException(as_tr.sbinDeserialize!(typeof(a)));
 }
